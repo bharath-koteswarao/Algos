ab97f736cdc39a1a79cd14e346ed10700d98c568
Pk <pavankoushik22@gmail.com>
2017-07-29 13:06:22 +0530

    Add files via upload

---
+++ src/python/string searching algos/KMP.py
@@ -0,0 +1,27 @@
+import time
+
+string=input('enter the string: ')
+substring=input('enter the sub string to be searched: ')
+
+
+
+
+
+
+
+
+
+
+
+
+#time calculation
+start=time.time()
+
+#implementation
+
+
+
+end=time.time()
+print('---time taken for this code is %f secs---'%(end-start))
+
+print('')
