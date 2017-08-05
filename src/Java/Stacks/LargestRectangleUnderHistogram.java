65d53282745ae95b2ba616fd1ad3201e409317c4
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-08-05 21:52:27 +0530

    Stock span problem

--- src/Java/Stacks/LargestRectangleUnderHistogram.java
+++ src/Java/Stacks/LargestRectangleUnderHistogram.java
@@ -1,7 +1,24 @@
 package Java.Stacks;
 
+import java.util.Stack;
+
 /**
  * Created by BK on 05-08-2017.
  */
 public class LargestRectangleUnderHistogram {
+    public static void main(String... args) {
+        int[] input = {6, 2, 5, 4, 5, 1, 6};
+        int maxarea = -1;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < input.length; i++) {
+            if (stack.isEmpty()) stack.push(i);
+            else {
+                if (input[stack.peek()] < input[i]) stack.push(i);
+                else {
+
+                }
+            }
+        }
+        System.out.println(stack + " " + maxarea);
+    }
 }
