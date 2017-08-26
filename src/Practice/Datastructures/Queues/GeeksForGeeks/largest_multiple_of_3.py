8d0bdec9c1a095457be453b29578143d1ceb7975
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-08-26 15:23:31 +0530

    largest multiple of 3

---
+++ src/Practice/Datastructures/Queues/GeeksForGeeks/largest_multiple_of_3.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+
+def main():
+    que1 = deque([])
+    que2 = deque([])
+    n = int(input())
+    nums = []
+    total = 0
+    for i in range(0, n):
+        nums.append(int(input()))
+        if nums[i] % 3 is 1:
+            que1.append(nums[i])
+        elif nums[i] % 3 is 2:
+            que2.append(nums[i])
+        total += nums[i]
+    amount_to_remove = total % 3
+
+    que1 = deque(sorted(que1))
+    que2 = deque(sorted(que2))
+    nums = sorted(nums)
+
+    if amount_to_remove is 0:
+        pass
+    elif amount_to_remove is 1:
+        nums.remove(que1.popleft())
+    elif amount_to_remove is 2:
+        if len(que2) is 0:
+            nums.remove(que1.popleft())
+            nums.remove(que1.popleft())
+        else:
+            nums.remove(que2.popleft())
+    answer = ""
+    for i in reversed(nums):
+        answer += str(i)
+    print(answer)
+
+
+main()
