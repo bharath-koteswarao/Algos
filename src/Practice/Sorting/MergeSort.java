7b98896e66f6e87dfddf84e7c3b956bc1d36fafd
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-09-12 23:20:15 +0530

    Merge Sort

--- src/Practice/Sorting/MergeSort.java
+++ src/Practice/Sorting/MergeSort.java
@@ -10,18 +10,25 @@ public class MergeSort {
 
     public static void main(String[] args) {
         merge = new MergeTwoSortedArrays();
-        int[] input = {4, 8, 12, 3, 13, 7, 1};
+        int[] input = {4, 2, 12, 3, 13, 7, 1};
+        bk.print_int(input);
         int[] sorted = mergeSort(input);
         bk.print_int(sorted);
     }
 
     public static int[] mergeSort(int[] input) {
-        int length = input.length;
         int[] sorted = new int[0];
-        if (length != 1) {
-            int[] left = leftArray(input, input.length / 2);
-            int[] right = rightArray(input, left.length);
+        int[] left = leftArray(input, input.length / 2);
+        int[] right = rightArray(input, left.length);
+        if (left.length!=1){
             mergeSort(left);
+        } else {
+            System.out.print("Left : ");
+            bk.print_int(left);
+            System.out.print("Right : ");
+            bk.print_int(right);
+        }
+        if (right.length!=1){
             mergeSort(right);
         }
         return sorted;
