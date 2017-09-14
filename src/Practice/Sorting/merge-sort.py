a9d50732efc6997f4ae31a8c3b969ebbcb0688cd
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-09-14 17:41:08 +0530

    Merge Sort

---
+++ src/Practice/Sorting/merge-sort.py
@@ -0,0 +1,44 @@
+def merge(arr, left, right):
+    i = 0
+    j = 0
+    k = 0
+    print("arr is ", arr, " left is ", left, " right is ", right)
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        k += 1
+        i += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        k += 1
+        j += 1
+    print("merged is ",arr)
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        merge(arr, left, right)
+
+
+def main():
+    unsorted = [3, 463, 435, 46, 39, 9, 2, 1]
+    print(unsorted)
+    merge_sort(unsorted)
+    print(unsorted)
+
+
+main()
