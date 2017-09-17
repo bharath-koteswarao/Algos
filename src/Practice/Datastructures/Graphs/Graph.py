af1bc84ec7a4a207adfac0c8572950fd406814a4
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-09-17 12:51:24 +0530

    Solved word ladder game

--- src/Practice/Datastructures/Graphs/Graph.py
+++ src/Practice/Datastructures/Graphs/Graph.py
@@ -8,10 +8,22 @@ class Graph:
     def addVertex(self, key):
         self.verticesList[key] = Vertex(key)
 
+    def __iter__(self):
+        return iter(self.verticesList.values())
+
+    def getAdjacencyList(self, key):
+        if type(key) is str:
+            return [self.getVertex(val) for val in list(self.getVertex(key).connections.keys())]
+            # These return statements simply return adjacency list with vertices in it
+        elif type(key) is Vertex:
+            return [self.getVertex(val) for val in list(key.connections.keys())]
+
+    def getVertex(self, key):
+        return self.verticesList[key]
+
     def addEdge(self, source, destination, weight):
         if source not in self.verticesList:
             self.verticesList[source] = Vertex(source)
         if destination not in self.verticesList:
             self.verticesList[destination] = Vertex(destination)
         self.verticesList[source].addNeighbor(destination, weight)
-
--- src/Practice/Datastructures/Graphs/graph-for-word-ladder-game.py
+++ src/Practice/Datastructures/Graphs/graph-for-word-ladder-game.py
@@ -1,6 +1,32 @@
 from Graph import Graph
 
 
+def findPath(source, destination, graph):   # This is bfs
+    i = 1  # Indicates level
+    for vertex in graph:
+        vertex.visited = False
+    levels = {}  # This stores the nodes visited in each level current level is i-1 = 0
+    current_level = [graph.getVertex(source)]  # Stores vertices of current level
+    while len(current_level) is not 0:
+        next_level = []  # Add all nodes adjacent to nodes of current level
+        for popped in current_level:
+            popped.visited = True  # Since we have visited this already
+            print(popped.key, " is visited")
+            for neighbor in graph.getAdjacencyList(popped):
+                if neighbor not in next_level and not neighbor.visited:
+                    neighbor.visited = True  # This is to prevent this node to be visited again
+                    print(neighbor.key)
+                    next_level.append(neighbor)
+        levels[i] = [vert.key for vert in next_level]
+        current_level = next_level
+        i += 1
+    print(levels)
+    for i in levels:
+        if destination in levels[i]:
+            print(destination, " arrived at ", i)
+            break
+
+
 def buildGraph(dictionary, graph):
     for collection in dictionary.keys():
         words = dictionary[collection]
@@ -9,8 +35,7 @@ def buildGraph(dictionary, graph):
                 if word1 != word2:
                     graph.addEdge(word1, word2, 0)
 
-    for vertex in graph.verticesList:
-        print(vertex, graph.verticesList[vertex].connections)
+    findPath("fool", "sage", graph)
 
 
 def main():
