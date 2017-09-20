730f519a036d753a7d230d4010ee5bfe63d64a1b
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-09-20 22:23:22 +0530

    Heap Node

---
+++ src/Practice/Datastructures/Heaps/heap_node.py
@@ -0,0 +1,9 @@
+class HeapNode:
+    data = None
+    left = None
+    right = None
+
+    def __init__(self, data, left, right):
+        self.data = data
+        self.left = left
+        self.right = right
