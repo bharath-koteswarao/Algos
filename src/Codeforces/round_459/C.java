c0959b15dfe7ca078f15fa5978ebcb720c19d03c
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2018-01-31 12:09:58 +0530

    Solved C in round 459

--- src/Codeforces/round_459/C.java
+++ src/Codeforces/round_459/C.java
@@ -8,18 +8,22 @@ public class C {
         String inp = sc.next();
         int ans = 0;
         for (int i = 0; i < inp.length(); i++) {
-            int score = 0, qmarks = 0;
+            int score = 0, qm = 0;
             for (int j = i; j < inp.length(); j++) {
                 if (inp.charAt(j) == '(') score += 1;
                 else if (inp.charAt(j) == ')') score -= 1;
-                else {
-                    if (qmarks >= score) score += 1;
-                    else qmarks += 1;
+                else qm += 1;
+                if (qm > 0 && qm > score) {
+                    qm -= 1;
+                    score += 1;
                 }
-                if (score < 0) break;
-                else if ((j - i) % 2 == 1) {
-                    if (score == 0 && qmarks == 0) ans += 1;
-                    else if (qmarks >= score) ans += 1;
+                if (score < 0 && qm <= 0) break;
+                else if (score < 0) {
+                    score += 1;
+                    qm += 1;
+                }
+                if ((j - i) % 2 == 1) {
+                    if (qm >= score) ans += 1;
                 }
             }
         }
