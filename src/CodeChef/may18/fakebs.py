6f9b630283903908c6db0ace4f105039a709ef74
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2018-05-14 09:26:16 +0530

    Fake bs modified

--- src/CodeChef/may18/fakebs.py
+++ src/CodeChef/may18/fakebs.py
@@ -15,49 +15,85 @@
 10 3 5 2 1 0 8
 1
 """
+
+
+class Node:
+    cl, cr = 0, 0
+    ans = 0
+
+    def __init__(self, val, idx):
+        self.val = val
+        self.idx = idx
+
+    def __repr__(self):
+        return '(' + str(self.val) + ',' + str(self.idx) + ',' + str(self.cl) + ',' + str(self.cr) + ')'
+
+
+def build(start, end, arr, idx, dic, tree):
+    if idx < len(tree):
+        if start == end:
+            dic[arr[start]] = Node(arr[start], idx)
+            tree[idx] = arr[start]
+        elif start <= end:
+            mid = (start + end) // 2
+            dic[arr[mid]] = Node(arr[mid], idx)
+            tree[idx] = arr[mid]
+            build(start, mid - 1, arr, 2 * idx + 1, dic, tree)
+            build(mid + 1, end, arr, 2 * idx + 2, dic, tree)
+
+
+def construct(start, end, node, arr, dic, tree):
+    if start <= end:
+        mid = (start + end) // 2
+        dic[arr[mid]] = Node(arr[mid], node)
+        tree[node] = arr[mid]
+        # print(mid + 1, node)
+        construct(start, mid - 1, 2 * node + 1, arr, dic, tree)
+        construct(mid + 1, end, 2 * node + 2, arr, dic, tree)
+
+
+def get_path(idx):
+    l = []
+    while idx != 0:
+        l.append(idx)
+        idx = (idx - 1) // 2
+    l.append(0)
+    return l[::-1]
+
+
 if __name__ == '__main__':
     for _ in range(int(input().strip())):
         n, q = [int(__) for __ in input().strip().split()]
         arr = [int(__) for __ in input().strip().split()]
-        idx = {}
-        sor = {}
-        std = sorted(arr)
-        for __ in range(n):
-            sor[std[__]] = __
-        for __ in range(n):
-            idx[arr[__]] = __
+        queries = []
         for __ in range(q):
-            x = int(input().strip())
-            lc, rc = sor[x], n - sor[x] - 1
-            ind = idx[x]
-            low, high = 0, n - 1
-            lswaps, rswaps = 0, 0
-            possible = True
-            while low <= high:
-                mid = (low + high) // 2
-                middle = arr[mid]
-                if x == middle:
-                    break
-                elif x < middle:
-                    if ind < mid:
-                        high = mid - 1
-                    else:
-                        if lc > 0:
-                            lswaps += 1
-                            lc -= 1
-                            low = mid + 1
-                        else:
-                            possible = False
-                            break
+            queries.append(int(input().strip()))
+        tree = [0 for i in range(4 * n)]
+        dic = {}
+        sor_dic = {}
+        so = sorted(arr)
+        for x in range(len(so)):
+            sor_dic[so[x]] = x
+        # build(0, n - 1, arr, 0, dic, tree)
+        construct(0, n - 1, 0, arr, dic, tree)
+        for val in dic:
+            cl, cr = 0, 0
+            path = get_path(dic[val].idx)
+            for i in range(len(path) - 1):
+                left = path[i + 1] == (2 * path[i] + 1)
+                nod = tree[path[i]]
+                if val > nod:
+                    cr += 1 if left else 0
                 else:
-                    if ind > mid:
-                        low = mid + 1
-                    else:
-                        if rc > 0:
-                            rswaps += 1
-                            rc -= 1
-                            high = mid - 1
-                        else:
-                            possible = False
-                            break
-            print(max(lswaps, rswaps) if possible else -1)
+                    cl += 1 if not left else 0
+            dic[val].cl = cl
+            dic[val].cr = cr
+        # print(dic)
+        for val in dic:
+            cl, cr = dic[val].cl, dic[val].cr
+            if sor_dic[val] >= cl and n - sor_dic[val] - 1 >= cr:
+                dic[val].ans = max(cl, cr)
+            else:
+                dic[val].ans = -1
+        for query in queries:
+            print(dic[query].ans)
