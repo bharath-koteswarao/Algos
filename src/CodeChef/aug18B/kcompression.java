b44502ab41e9a693ef20ea3b7a08f5c1c70055d1
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2018-08-13 16:04:03 +0530

    kcomp gave wrong answer

--- src/CodeChef/aug18B/kcompression.java
+++ src/CodeChef/aug18B/kcompression.java
@@ -1,13 +1,15 @@
 package CodeChef.aug18B;
 
-/**
- * Created by bk on 09-08-2018.
- */
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+
+/**
+ * Created by bk on 09-08-2018.
+ */
 
 class FastIO {
     private BufferedReader br;
@@ -52,29 +54,97 @@ class FastIO {
     }
 }
 
+class Pair implements Comparable<Pair> {
+    int idx;
+    int val;
+
+    Pair(int idx, int val) {
+        this.idx = idx;
+        this.val = val;
+    }
+
+    @Override
+    public int compareTo(Pair o) {
+        if (this.val == o.val) return this.idx - o.idx;
+        else return this.val - o.val;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.idx + ", " + this.val + ")";
+    }
+}
+
 public class kcompression {
     private static int[] arr;
 
     public static void main(String[] __) {
         FastIO sc = new FastIO();
-        int n = sc.nextInt();
-        arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = i;
-        System.out.println(answer(0, arr.length));
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt(), sum = sc.nextInt();
+            arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+//            System.out.println(answer(0, arr.length, sum) + 1);
+            check(1, sum);
+        }
     }
 
-    private static boolean check(int n) {
-        return n <= 15;
+    private static boolean check(int n, int sum) {
+        if (n == 0) return arr.length <= sum;
+        else {
+            int[] ans = new int[arr.length];
+            for (int i = 0; i < ans.length; i++) {
+                if (ans[i] == 0) find(i, ans, n);
+            }
+            System.out.println(Arrays.toString(ans));
+            int su = 0;
+            for (int el : ans) su += el;
+            return su <= sum;
+        }
     }
 
-    private static int answer(int start, int end) {
+    private static void find(int idx, int[] ans, int n) {
+        PriorityQueue<Pair> heap = new PriorityQueue<>();
+        int last = Math.min(idx + n, ans.length - 1);
+        for (int i = idx; i <= last; i++) {
+            if (arr[i] <= arr[idx] && ans[i] == 0) {
+                heap.add(new Pair(i, arr[i]));
+            }
+        }
+        Pair top = heap.peek();
+        if (top != null && top.idx == idx) {
+            int start = Math.max(0, idx - n);
+            int end = Math.min(idx + n, ans.length - 1);
+            int ma = 0;
+            for (int i = start; i <= end; i++) {
+                if (arr[i] < arr[idx]) ma = Math.max(ma, ans[i]);
+            }
+            ans[top.idx] = ma + 1;
+        } else if (heap.size() == 1) {
+            int start = Math.max(0, idx - n);
+            int end = Math.min(idx + n, ans.length - 1);
+            int ma = 0;
+            for (int i = start; i <= end; i++) {
+                if (arr[i] < arr[idx]) ma = Math.max(ma, ans[i]);
+            }
+            ans[heap.remove().idx] = ma + 1;
+        } else {
+            while (!heap.isEmpty()) {
+                Pair cur = heap.remove();
+                find(cur.idx, ans, n);
+            }
+        }
+    }
+
+    private static int answer(int start, int end, int sum) {
         int mid = (start + end) / 2;
         while (start != end) {
-            if (check(mid)) {
+            if (check(mid, sum)) {
                 start = mid + 1;
             } else end = mid - 1;
             mid = (start + end) / 2;
         }
-        return check(start) ? start : start - 1;
+        return check(start, sum) ? start : start - 1;
     }
 }
